d9a6c2f4f35d76a97da5b4901d5da909c8333f9e
Olivia Yu <olivyu@gmail.com>
2025-03-01 12:17:26 -0800

    Clean up skills

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -9,6 +9,7 @@ const PROJECTS_INFO = [
       "Waddle is basically a clone of NYT's popular Wordle game. Players can guess the 5-letter word and receive clues about the letter placement accuracy.",
     repoLink: "https://github.com/oliv-yu/waddle",
     demoLink: "https://oliv-yu.github.io/waddle/",
+    skills: ["React", "SCSS", "HTML"],
   },
   {
     imagePath: "/images/boredapp.jpg",
@@ -17,6 +18,14 @@ const PROJECTS_INFO = [
       "Bored is a simple dashboard of what to do when bored. Widgets include a weather report, a random activity generator, and a local business suggester.",
     repoLink: "https://github.com/oliv-yu/bored",
     demoLink: "https://oliv-yu.github.io/bored",
+    skills: [
+      "React",
+      "HERE API",
+      "Open Weather Map API",
+      "Moment",
+      "Axios",
+      "SCSS",
+    ],
   },
   {
     imagePath: "/images/doodle.png",
@@ -25,6 +34,7 @@ const PROJECTS_INFO = [
       "Doodle is a straight-forward drawing app similar to Microsoft Paint, where a painter can alter brush width and undo changes.",
     repoLink: "https://github.com/oliv-yu/doodle",
     demoLink: "https://oliv-yu.github.io/doodle",
+    skills: ["Javascript", "CSS", "Next.js"],
   },
   {
     imagePath: "/images/interactivecomments.jpg",
@@ -33,18 +43,19 @@ const PROJECTS_INFO = [
       "Interactive Comments is a frontendmentor.io challenge where users can comment, reply to, and upvote each other, as well as delete their own responses.",
     repoLink: "https://github.com/oliv-yu/interactive-comments",
     demoLink: "https://oliv-yu.github.io/interactive-comments",
+    skills: ["React", "SCSS", "HTML"],
   },
 ];
 
 function Link({ type, href }) {
   const linkData = {
     repo: {
-      dimensions: "h-6 w-6",
+      dimensions: "h-5 w-5",
       icon: "github",
       text: "Source Code",
     },
     demo: {
-      dimensions: "h-6 w-9",
+      dimensions: "h-5 w-7",
       icon: "laptop",
       text: "Live Demo",
     },
@@ -63,7 +74,7 @@ function Link({ type, href }) {
           cssClass={"inline fill-default-50 " + linkData[type].dimensions}
         />
         <span>{linkData[type].text}</span>
-        <Icon icon="arrowup" cssClass={"inline fill-default-50 h-3 w-3"} />
+        <Icon icon="arrowup" cssClass="inline fill-default-50 h-3 w-3" />
       </div>
     </a>
   );
@@ -74,7 +85,7 @@ export default function Projects() {
     <div className="projects-content">
       <ul>
         {PROJECTS_INFO.map(
-          ({ imagePath, title, description, demoLink, repoLink }) => (
+          ({ imagePath, title, description, demoLink, repoLink, skills }) => (
             <li className="mb-2" key={title}>
               <div
                 className="z-0 rounded-md transition motion-reduce:transition-none hover:bg-default-950/45"
@@ -83,7 +94,7 @@ export default function Projects() {
                 <div className="group opacity-100 relative grid gap-4 transition-all sm:grid-cols-6 sm:gap-8 md:gap-4 p-[20px]">
                   <div className="z-10 sm:col-span-2">
                     <img
-                      className="w-full h-[180px] object-cover rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30"
+                      className="w-full h-[200px] object-cover rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30"
                       alt={`${title} Card`}
                       src={imagePath}
                     />
@@ -97,7 +108,20 @@ export default function Projects() {
 
                     <p className="mt-2 text-sm leading-normal">{description}</p>
 
-                    <div className="mt-6 flex gap-x-2">
+                    <div className="flex flex-wrap text-xs uppercase space-x-1 mt-2">
+                      {skills.map((item) => (
+                        <p
+                          className="my-2"
+                          key={`${title}-${item}-${Math.random()}`}
+                        >
+                          <span className="rounded-3xl border-1 border-slate-400 text-slate-400 py-1 px-2 hover:bg-slate-800 transition ease-in-and-out duration-100 transition-colors">
+                            {item}
+                          </span>
+                        </p>
+                      ))}
+                    </div>
+
+                    <div className="mt-4 flex gap-x-2">
                       <Link type="repo" href={repoLink} />
 
                       <Link type="demo" href={demoLink} />
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,28 +1,36 @@
 import React from "react";
 
 const SKILLS_LIST = [
-  "JavaScript",
-  "React",
-  "CSS3",
-  "HTML5",
-  "Git",
-  "Tailwind",
-  "Sass",
-  "Playwright",
+  { skills: ["JavaScript", "TypeScript", "HTML"], title: "Languages" },
+  { skills: ["CSS", "Tailwind", "Sass"], title: "Styling" },
+  { skills: ["Playwright", "Jest"], title: "Testing" },
+  { skills: ["ReactJS", "Node.js", "Storybook"], title: "Tools" },
+  { skills: ["Git"], title: "Version Control" },
 ];
 
 export default function Skills() {
   return (
-    <div className="skills-content flex flex-wrap font-mono text-xs font-bold">
-      {SKILLS_LIST.map((item) => {
-        return (
-          <p className="my-4" key={`skill-${item}`}>
-            <span className="rounded-3xl border-2 border-default-400 py-2 px-3 mr-3 hover:bg-default-900 transition ease-in-and-out duration-300 transition-colors">
-              {item}
-            </span>
-          </p>
-        );
-      })}
+    <div className="skills-content">
+      {SKILLS_LIST.map(({ title, skills }) => (
+        <div key={title} className="grid grid-cols-4">
+          <div className="text-sm text-default-300 flex items-center align-right uppercase">
+            {title}
+          </div>
+
+          <div className="col-span-3 flex flex-wrap font-mono text-xs font-bold space-x-4 space-y-4">
+            {skills.map((item) => (
+              <p
+                className="my-4 cursor-pointer hover:scale-125 hover:text-md hover:tracking-wider hover:ease-in-and-out transition"
+                key={`skill-${item}`}
+              >
+                <span className="rounded-3xl border-2 border-default-400 py-2 px-3 hover:bg-default-500 transition ease-in-and-out duration-300 transition-colors">
+                  {item}
+                </span>
+              </p>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
