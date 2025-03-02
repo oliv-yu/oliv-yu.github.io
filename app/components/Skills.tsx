1caf007960d85ea0e2d096e9051919f4744dea91
Olivia Yu <olivyu@gmail.com>
2025-03-02 15:16:19 -0800

    Avoid overlapping text when resizing

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -12,18 +12,18 @@ export default function Skills() {
   return (
     <div className="skills-content">
       {SKILLS_LIST.map(({ title, skills }) => (
-        <div key={title} className="grid grid-cols-4">
+        <div key={title} className="grid sm:grid-cols-4 mb-3 sm:mb-0">
           <div className="text-sm text-default-300 flex items-center align-right uppercase">
             {title}
           </div>
 
-          <div className="col-span-3 flex flex-wrap font-mono text-xs font-bold space-x-4 space-y-4">
+          <div className="flex flex-wrap font-mono text-xs space-x-3 sm:space-x-4 sm:col-span-3">
             {skills.map((item) => (
               <p
                 className="my-4 cursor-pointer hover:scale-125 hover:text-md hover:tracking-wider hover:ease-in-and-out transition"
                 key={`skill-${item}`}
               >
-                <span className="rounded-3xl border-2 border-default-400 py-2 px-3 hover:bg-default-500 transition ease-in-and-out duration-300 transition-colors">
+                <span className="rounded-3xl border-1 border-default-400 py-2 px-3 hover:bg-default-500 transition ease-in-and-out duration-300 transition-colors">
                   {item}
                 </span>
               </p>
